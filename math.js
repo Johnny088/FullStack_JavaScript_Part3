5c8ebabb6bdcb06630daacb313f9eab4f670bae2
Johnny <esp25goodlife@gmail.com>
2025-11-30 12:38:25 +0200

    fixed

--- math.js
+++ math.js
@@ -1,37 +1,29 @@
 //Створіть функцію `getRandomInt`, яка приймає два цілих числа: `min` та `max`. Ця функція повинна генерувати випадкове ціле число в діапазоні від `min` до `max` (включно).
-function getRandomInt() {
-  let min = Number(prompt(`enter the min number`)); // number(prompt(`enter the min number`));
-  let max = Number(prompt(`enter the max number`));
-  console.log(min);
-  console.log(max);
-  if (min > max) {
-    let temp = min;
-    min = max;
-    max = temp;
-  }
-  let x = Math.floor(Math.random() * 100 + 1);
+function getRandomInt(min, max) {
   return `your random number in range from ${min} to ${max} is: ${Math.floor(
     Math.random() * (max - min + 1) + min
   )}`;
 }
-console.log(getRandomInt());
+console.log(getRandomInt(0, 1));
+console.log(getRandomInt(1, 10));
+console.log(getRandomInt(40, 50));
+console.log(getRandomInt(1, 100));
 
 // Ваше завдання - створити функцію greet в JavaScript.
-let name = prompt(`enter your name:`);
-const greeting = 'Hello, ';
-function greetByName(greeting, name) {
-  return greeting + name;
+function greetByName(msg, name) {
+  return msg + ', ' + name;
 }
-
-console.log(greeting, name);
+console.log(greetByName('hi', 'testName'));
+console.log(greetByName('hey', 'testName'));
+console.log(greetByName('hello', 'testName'));
 
 //Створіть функцію sumBigIntegers, яка приймає два рядки (numStr1 та numStr2), що представляють великі числа.
 // Функція повинна перетворити ці рядки на BigInt і повернути їх суму.
 let number1 = '9007199254740991';
 let number2 = '9007199254740991';
 
-function sumBigIntegers(number1, number2) {
-  let resultNumber = BigInt(number1) + BigInt(number2);
+function sumBigIntegers(numStr1, numStr2) {
+  let resultNumber = BigInt(numStr1) + BigInt(numStr2);
   return resultNumber;
 }
 
