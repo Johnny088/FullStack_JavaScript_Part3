376424b4f172d530b2c887a67916778211b0a6c3
Johnny <esp25goodlife@gmail.com>
2025-12-11 13:51:09 +0200

    added two tasks

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // ============task 1 ===================
-let userObj = { firstName: 'Johnny', lastName: 'Smith', age: 25 };
+let userObj = {
+  firstName: 'Johnny',
+  lastName: 'Smith',
+  age: 25,
+};
 console.log(userObj);
 
 // task 2 =====================
 
-userObj.fullName() = userObj.firstName + ' ' + userObj.lastName;
+userObj.fullName = function () {
+  return userObj.firstName + ' ' + userObj.lastName;
+};
 console.log(userObj.fullName());
