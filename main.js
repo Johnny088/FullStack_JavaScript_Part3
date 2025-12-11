9f245cbbf89cdd692eb3858a225fb9774278846e
Johnny <esp25goodlife@gmail.com>
2025-12-11 11:57:00 +0200

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+// ============task 1 ===================
+let userObj = { firstName: 'Johnny', lastName: 'Smith', age: 25 };
+console.log(userObj);
+
+// task 2 =====================
+
+userObj.fullName() = userObj.firstName + ' ' + userObj.lastName;
+console.log(userObj.fullName());
